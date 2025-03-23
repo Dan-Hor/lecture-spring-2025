197d13a47462e8ef35c6d5a138634173c4dbe59c
Daniel Horvath <danielhorvath1.dh@gmail.com>
2025-03-23 14:59:24 +0100

    Added plot output in main and newplot.py

---
+++ newplot.py
@@ -0,0 +1,7 @@
+import plotly.graph_objects as go
+
+def plot (a, b, c, d ,e ,f):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x = a, y = b, name = c))
+    fig.update_layout(title = d, xaxis_title = e, yaxis_title = f)
+    fig.show()
