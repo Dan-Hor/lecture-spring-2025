7e91eb27788f3a59dcd9430ebed6438dfa680487
Daniel Horvath <danielhorvath1.dh@gmail.com>
2025-03-23 15:14:30 +0100

    Reformatted with Ruff.

--- newplot.py
+++ newplot.py
@@ -1,7 +1,8 @@
 import plotly.graph_objects as go
 
-def plot (a, b, c, d ,e ,f):
+
+def plot(a, b, c, d, e, f):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x = a, y = b, name = c))
-    fig.update_layout(title = d, xaxis_title = e, yaxis_title = f)
+    fig.add_trace(go.Scatter(x=a, y=b, name=c))
+    fig.update_layout(title=d, xaxis_title=e, yaxis_title=f)
     fig.show()
